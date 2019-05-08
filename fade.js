fa4e111990c095b283a0a50f1666fbe9b7e8c3c7
Araya Wongwan <hi.arayaa@gmail.com>
2019-05-08 12:37:26 +0700

    added files

---
+++ fade.js
@@ -0,0 +1,10 @@
+const main = document.querySelector("section.main")
+
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(function (){
+//     main.style.display = "block"
+    main.style.opacity = 1
+    main.style.zIndex = 1
+  }, 7000)
+
+})
