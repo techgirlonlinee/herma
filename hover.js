9cd638a4d7439d618f1a548d28bb88545b070e26
Araya Wongwan <hi.arayaa@gmail.com>
2019-05-11 21:31:33 +0700

    update sounds

--- hover.js
+++ hover.js
@@ -7,30 +7,30 @@ const circleE = document.querySelector("section.e")
 
 circleA.addEventListener("mouseenter", function(){
 	const audio = document.createElement("audio")
-	audio.setAttribute("src", "sound/track1.wav")
+	audio.setAttribute("src", "sound/sample1.mp3")
 	audio.play()
 })
 
 circleB.addEventListener("mouseover", function(){
 	const audio = document.createElement("audio")
-	audio.setAttribute("src", "sound/track2.wav")
+	audio.setAttribute("src", "sound/sample2.mp3")
 	audio.play()
 })
 
 circleC.addEventListener("mouseover", function(){
 	const audio = document.createElement("audio")
-	audio.setAttribute("src", "sound/track33.wav")
+	audio.setAttribute("src", "sound/sample3.mp3")
 	audio.play()	
 })
 
 circleD.addEventListener("mouseover", function(){
 	const audio = document.createElement("audio")
-	audio.setAttribute("src", "sound/track4.wav")
+	audio.setAttribute("src", "sample4.mp3")
 	audio.play()	
 })
 
 circleE.addEventListener("mouseover", function(){
 	const audio = document.createElement("audio")
-	audio.setAttribute("src", "sound/track5.wav")
+	audio.setAttribute("src", "sample5.mp3")
 	audio.play()	
 })
