e1a441c05b4aa855eeb9b3e66b923141591d58c9
Araya Wongwan <hi.arayaa@gmail.com>
2019-05-11 12:02:07 +0700

    updates

--- autoscroll.js
+++ autoscroll.js
@@ -1,24 +1,3 @@
-// var scroll = setInterval(function(){ 
-// 	window.scrollBy(50, 10000);
-// 	}, 10000);
-
-// function scrollDown() { 
-//    scroll = setInterval(function(){ 
-//    	window.scrollBy(50, 1000); 
-//    	// console.log('start');
-//    }, 1500);
-// }
-
-// scrollDown()
-
-// function stop_scroll_down() {
-//    clearInterval(scroll);
-//    console.log('stop');
-// }
-
-
-
-
 function scrollY(speed) {
     $('html, body').animate({ scrollTop: $(document).height() - $(window).height() }, speed, function(
     	) {
@@ -30,20 +9,6 @@ speed = 12000;
 
 scrollY(speed)
 setInterval(function(){scrollY(speed)}, speed * 2);
-
-
-
-// function scrollX(speed) {
-//     $('html, body').animate({ scrollLeft: $(document).width() - $(window).width() }, speed, function(
-//     	) {
-//         $(this).animate({ scrollLeft: 0 }, speed);
-//     });
-// }
-
-// speed = 11500;
-// scrollX(speed)
-// setInterval(function(){scrollX(speed)}, speed * 2);
-
 
 
 function scrollX(speed) {
